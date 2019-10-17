240de1c5bcb78862a35cc4aed857c3793c8868d9
ShalomGottesman <shalomgottesman@gmail.com>
2019-10-17 19:04:50 -0400

    package and class to test if there is an active internet connection

---
+++ TeachHub/src/main/java/internet/InternetConnection.java
@@ -0,0 +1,35 @@
+package internet;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class InternetConnection {
+	private URL urlToTest;
+	
+	public InternetConnection() {
+		try {
+			this.urlToTest = new URL ("https://google.com");
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public InternetConnection(URL urlToTest) {
+		this.urlToTest= urlToTest;
+	}
+	
+	public boolean isConnectionAvailable() {
+		try {
+			final URLConnection con = this.urlToTest.openConnection();
+			con.connect();
+			con.getInputStream().close();
+			return true;
+		} catch (MalformedURLException e) {
+			return false;
+		} catch (IOException e) {
+			return false;
+		}
+	}
+}
